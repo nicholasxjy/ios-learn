a29a6bb9ca51a3f3d9247abbc59b127b0acc9bb8
nicholasxjy <yitaqi2011@gmail.com>
2014-08-04 00:28:14 +0800

    swift demo

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,35 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+let CONST = "hello const"
+
+var varible = 8
+
+var nameStr:NSString = "helloworld"
+
+let billion = 1_0000_0000
+
+let f_num:Float = 1
+
+let num_b:Int = Int(1.8)
+
+let result = (isSuccess:true, nickName:"nicholas", gender:"男")
+
+result
+result.isSuccess
+result.gender
+
+let rangA = 1...6
+
+for index in rangA
+{
+    index
+}
+
+
+
+
+
